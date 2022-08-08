697df59414e12306ed6e491b9a17af25a69ef513
Josue <manekenpix@fastmail.com>
2022-08-07 22:38:16 -0400

    feat: add docker and config files

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export default () => ({
   port: parseInt(process.env.PORT || '', 10) || 3000,
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import AppModule from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('main.ts');
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('port');
+
   await app.listen(port);
+  logger.log(`App started on port ${port}`);
 }
 bootstrap();
