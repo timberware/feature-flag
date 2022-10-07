a1247a8e295c37e932302ab9989549a4324f99c3
Chris Pinkney <hey@chrispinkney.com>
2022-10-06 20:27:01 -0400

    chore:project restructure

---
+++ web/svelte.config.js
@@ -0,0 +1,11 @@
+// svelte.config.js
+import preprocess from 'svelte-preprocess';
+
+const config = {
+  preprocess: preprocess({
+    // ...svelte-preprocess options
+  }),
+  // ...other svelte options
+};
+
+export default config;
