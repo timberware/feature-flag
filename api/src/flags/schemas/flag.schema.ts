630c246e1c17eb8d3ca873963ae4aad6dea0436d
Josue <manekenpix@fastmail.com>
2022-11-02 00:06:41 -0400

    refactor(api): add environment prop to flags and improve flag guard for saveFlag

--- api/src/flags/schemas/flag.schema.ts
+++ api/src/flags/schemas/flag.schema.ts
@@ -1,21 +1,27 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type FlagType = 'bool' | 'string' | 'number';
+export type FlagType = 'boolean' | 'string' | 'number';
 export type FlagValueType = string | number | boolean;
+export type FlagEnv = 'staging' | 'production';
 
 @Schema()
 export class Flag {
-  @Prop({ type: String, index: { unique: true } })
+  @Prop({ type: String })
   name!: string;
 
-  @Prop()
+  @Prop({ type: String })
   type!: FlagType;
 
   @Prop({ type: String || Number || Boolean })
   value!: FlagValueType;
+
+  @Prop({ type: String })
+  environment!: FlagEnv;
 }
 
 export type FlagDocument = Flag & Document;
 
 export const FlagSchema = SchemaFactory.createForClass(Flag);
+
+FlagSchema.index({ name: 1, environment: 1 }, { unique: true });
