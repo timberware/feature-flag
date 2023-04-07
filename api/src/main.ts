ceaa0cc4eab29402624d3ddad9a6dbb0f592d932
Josue <manekenpix@fastmail.com>
2023-04-07 00:52:18 -0400

    refactor: changes

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import AppModule from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('main.ts');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
