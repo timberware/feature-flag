853cc357dae30154313b938fa5709ae87afca1c8
Josue <manekenpix@fastmail.com>
2023-04-20 22:40:41 -0400

    feat(web): add enable/disable functionality

--- web/src/lib/Flags/Flag.svelte
+++ web/src/lib/Flags/Flag.svelte
@@ -9,11 +9,44 @@
   .section {
     width: 18%;
   }
+
+  .button-wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
 
 <script lang="ts">
+  import { env } from '$env/dynamic/public';
   import Button from '$lib/Button/Button.svelte';
   export let flag: FlagDataType;
+
+  const handleClick = async () => {
+    try {
+      const res = await fetch(`${env.PUBLIC_API_URL}:3000/flags/setEnabled`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          id: flag.id,
+          isEnabled: !flag.isEnabled
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      });
+
+      const { _id, isEnabled } = await res.json();
+
+      if (res.status === 200) {
+        flag = {
+          ...flag,
+          id: _id,
+          isEnabled
+        };
+      }
+    } catch (e) {
+      console.error({ e });
+    }
+  };
 </script>
 
 <div class="wrapper">
@@ -21,8 +54,8 @@
   <div class="section">{flag.type}</div>
   <div class="section">{flag.value}</div>
   <div class="section">{flag.environment}</div>
-  <div class="section">
-    <Button onClick="{() => console.log('Disable button')}">Disable</Button>
+  <div class="section button-wrapper">
+    <Button onClick="{handleClick}">{flag.isEnabled ? 'Disable' : 'Enable'}</Button>
     <Button onClick="{() => console.log('Remove button')}" buttonTheme="secondary"
       >Remove</Button
     >
